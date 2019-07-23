5adf3a60aea6075320bd353071c155f0a0c76dcc
A-Mozeak <alex@astar.app>
2019-07-23 18:23:50 -0400

    Added gesture detection.

--- dice_proj/lib/main.dart
+++ dice_proj/lib/main.dart
@@ -6,6 +6,7 @@ void main() => runApp(
           backgroundColor: Colors.red,
           appBar: AppBar(
             title: Text('Dice Roller'),
+            backgroundColor: Colors.red,
           ),
           body: DicePage(),
         ),
@@ -14,7 +15,26 @@ void main() => runApp(
 
 class DicePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Row(children: <Widget>[],);
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('Left button pressed.');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('Right button pressed.');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
